a59617a6e58f1c0b4e8245d5a2dda92146beae82
israel5000 <jayisrael813@gmail.com>
2025-07-03 16:19:48 +0100

    javascipt

--- week5/page-javascript/gessing-game/index.js
+++ week5/page-javascript/gessing-game/index.js
@@ -2,9 +2,66 @@
 let randomNumber=Math.floor(Math.random()   *   100)+1
 const guesses=document.querySelector(".guesses")
 const lastResult=document.querySelector(".lastResult")
-const lowOrHigh=document.querySelector(lowOrHigh)
-const getResult=document.querySelector(getResult)
+const lowOrHigh=document.querySelector(".lowOrHigh")
+const guesseSubmit=document.querySelector("guesseSubmit")
 
-const getFilled=document.querySelector(getFilled)
-let getCount=1
-let resetButton=
+const guessField=document.querySelector(".guessfield")
+let guessCount=1
+let resetButton;
+
+function checkGuess(){
+    // console.log("I am a placeholder")
+    const userGuess = Number(gessesfield.value);
+    // conditional if statement
+    if(guessCount==1){
+        guesses.textContent="previous guesses:"
+    }
+    guessCount.textContent`${guessCount.textContent} ${userGuess}`;
+    //another if  conditional statement
+    if(userGuess===randomNumber){
+        lastResult.textContent="Congratulations! you got it right"
+        lastResult.style.backgroundColour="green"
+        lowOrHigh.textContent="";
+        setGameOver()
+    }else if(guessCount==10){
+    lastResult.textContent="!!!GAME OVER!!!";
+    lowOrHigh.textContent="";
+    setGameOver()
+    }else{
+        lastResult.textContent="run";
+        lastResult.style.backgroundColour="red";
+        if(userGuess<randomNumber){
+        lowOrHigh.textContent="last guess was too low";
+     }else if(userGuess>randomNumber){
+        lowOrHigh.textContent="last guess was too high"
+     }
+    }
+    guessCount++
+    guessfield.value="";
+    gessesfield.focus();
+    guesseSubmit.addEventListener("click",checkGuess)
+}
+function setGame(){
+    guessField.disable=true;
+    guesseSubmit.disable=true
+    resetButton=document.createElement("button")
+    resetButton.textContent="start new game"
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame)
+}
+function resetGame(){
+    guessCount=1
+    const resetParas=document.querySelectorAll(".resetParas-p")
+    // for loop
+    for(const resetPara of resetParas){
+        resetPara.textContent=""
+    }
+    resetButton.parentNode.remove(resetButton);
+    guessField.disable=false;
+    guesseSubmit.disable=false;
+    guessField.value="";
+    guessField.focus()
+    lastResult.style.backgroundColour="white";
+    randomNumber=Math.floor(Math.random()*100) + 1
+}
+console.log(guesses)
